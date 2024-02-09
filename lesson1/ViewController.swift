b988e858205af578aea5db16e2c3acedf4dc64ef
jGulzat <gulzatj97@mail.ru>
2024-02-09 20:23:22 +0600

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Gulzat Zheenbek kyzy on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
